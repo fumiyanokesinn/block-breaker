3d1e55f54e4ea4fe76d9648acbad5b6f0f900c67
shindori fumiya <shindori238@gmail.com>
2023-05-08 23:01:56 +0900

    今日の作業

--- Ball.gd
+++ Ball.gd
@@ -1,26 +1,33 @@
 extends RigidBody2D
 
-const MAX_SPEED = 900
+const MAX_SPEED = 800
 @export var speed_up = 10
 @export var ball_speed = 430
 @export var mode = 0;
 var direction:Vector2
 var velocity:Vector2
-var paddle:Node
+var paddle:CharacterBody2D
+var blockHit:Node
+var wallHit:Node
 @onready var ballPosition = position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	paddle = get_parent().get_node("Bar")
+	blockHit = get_parent().get_node("BlockHit")
+	wallHit = get_parent().get_node("WallHit")
 
 func _on_body_entered(body):
 	ball_speed += speed_up
 	direction = linear_velocity.normalized() # 追加
 	velocity = direction *  min(ball_speed, MAX_SPEED)
 	if body.is_in_group("Blocks"):
+		blockHit.play()
 		body.hide()
 		body.set_collision_layer_value(1,false)
 		body.set_collision_mask_value(1,false)
+	else :
+		wallHit.play()
 	if body.get_name() == "Bar":
 		direction = (position - body.position).normalized()
 		velocity = direction *  min(ball_speed, MAX_SPEED)
